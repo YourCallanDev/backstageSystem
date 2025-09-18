daca308b95a6b97be697ab491a5573d1eb8ba6ff
YourCallanDev <107786341+YourCallanDev@users.noreply.github.com>
2025-09-18 20:03:39 +0100

    Create script-index.js

---
+++ script-index.js
@@ -0,0 +1,6 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+  // Open 2 display tabs + control tab
+  window.open("display.html", "_blank");
+  window.open("display.html", "_blank");
+  window.open("control.html", "_blank");
+});
